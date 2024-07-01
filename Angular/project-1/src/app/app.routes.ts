0c4d59b0be6f8d40bdbbb102efe06dc36e77d1e6
Jared Mai <jaredmai2@gmail.com>
2024-07-01 00:57:33 -0500

    Footer Styling, Navigation setup
    Added styling for footer, nagivation bar now successfully routes page to correct component.

--- Angular/project-1/src/app/app.routes.ts
+++ Angular/project-1/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { OfficesComponent } from './offices/offices.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+
+    {
+        path: 'employees',
+        component: EmployeesComponent
+    },
+
+    {
+        path: 'offices',
+        component: OfficesComponent
+    }
+];
